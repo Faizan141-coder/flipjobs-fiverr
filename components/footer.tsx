eba3fd3b64e51e02181e5ca0296a425054ed338c
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-04-11 17:40:07 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center text-center">
       <div className="flex p-9">
         <h1 className="cursor-pointer text-green-950">
           Große Str. 53, 47559 Kranenburg, Germany
